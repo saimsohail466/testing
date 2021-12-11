11e874b838c6c676d784afa4ee6ae2eeafb777bf
saimsohail466 <saimsohail466@gmail.com>
2021-12-12 03:39:15 +0500

    integrate device and cancancan gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_141118) do
+ActiveRecord::Schema.define(version: 2021_12_11_223318) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.integer "User_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["User_id"], name: "index_posts_on_User_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "deleted_at"
+    t.string "email"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "Users"
 end
